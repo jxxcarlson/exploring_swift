bda7593e7210f8c5dddcffcd62e02815dd603767
James Carlson <jxxcarlson@mac.com>
2015-06-14 20:23:21 -0400

    Add graphicsHelloWorldIOS

---
+++ graphicsHelloWorldIOS.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+func drawFigure(frame: CGRect) {
+    
+    let bgColor = UIColor(red: 0.58, green: 0.5,
+        blue: 0.5, alpha: 1.0)
+    let fgColor = UIColor(red: 1.0, green: 0.0,
+        blue: 0.4, alpha: 1.0)
+    let r1 =  CGRect(x: 40, y: 40,
+        width: 60, height: 60)
+    
+    bgColor.setFill()
+    UIRectFill(frame)
+    
+    fgColor.setFill()
+    UIRectFill(r1)
+}
+
+class GView: UIView {
+    override func drawRect(dirtyRect: CGRect) {
+        drawFigure(self.frame)
+    }
+}
+
+let view = GView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
