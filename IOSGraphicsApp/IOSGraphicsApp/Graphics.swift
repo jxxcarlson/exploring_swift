4262cb6ca8e293d8d5593b2c2ab30aa504446ef3
James Carlson <jxxcarlson@mac.com>
2015-06-22 19:38:52 -0400

    refactor IOSGraphicsApp, update manifest

--- IOSGraphicsApp/IOSGraphicsApp/Graphics.swift
+++ IOSGraphicsApp/IOSGraphicsApp/Graphics.swift
@@ -8,20 +8,27 @@
 
 import UIKit
 
-let S1 = Shape(x: 0, y: 0, width: 200, height: 200)
-let S2 = Shape(x: 100, y: 100, width: 200, height: 200)
+func drawFigure(frame: CGRect) {
+    
+    let g:CGFloat = 0.85
+    let bgColor = UIColor(red: g, green: g, blue: g, alpha: 1.0)
+    bgColor.set()
+    UIRectFill(frame)
+
+    let S1 = Shape(x: 0, y: 0, width: 200, height: 200)
+    let S2 = Shape(x: 100, y: 100, width: 200, height: 200)
+    
+    S1.draw(frame)
+    S2.draw(frame)
+    
+    
+}
 
 class GraphicsView: UIView {
     
     override func drawRect(rect: CGRect) {
         
-        let g:CGFloat = 0.85
-        let bgColor = UIColor(red: g, green: g, blue: g, alpha: 1.0)
-        bgColor.set()
-        UIRectFill(rect)
-        
-        S1.draw(frame)
-        S2.draw(frame)
+        drawFigure(frame)
         
     }
     
